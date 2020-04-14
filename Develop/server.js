853ae22231442f23aa866df511e61fa3b200578c
Ryan Tam <ryanjtam@gmail.com>
2020-04-14 14:08:06 -0700

    adding files and framing the routes files

---
+++ Develop/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+});
