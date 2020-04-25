67acac744d66db7de20a38ba0bdae03f884678a2
Ryan Tam <ryanjtam@gmail.com>
2020-04-25 03:56:21 -0700

    update api route to include information about the get mongo query

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,6 +1,7 @@
 const Workout = require("../models/workout.js");
 
 module.exports = function(app) {
+    //get all workout information
     app.get("/api/workouts", (req, res) => {
         Workout.find({})
             .then(data => {
@@ -11,6 +12,7 @@ module.exports = function(app) {
             });
     });
     
+    // create new workout
     app.post("/api/workouts", ({ body }, res) => {
         Workout.create(body)
             .then(data => {
@@ -22,26 +24,25 @@ module.exports = function(app) {
             });
     });
     
+    // add exercises to workout
     app.put("/api/workouts/:id", (req, res) => {
         Workout.updateOne(
-            {   _id: req.params.id  },
-            { 
-                $push: { exercises: req.body },
-                $inc: { totalDuration: req.body.duration }
-            }
+            { _id: req.params.id  },
+            { $push: { exercises: req.body }, $inc: { totalDuration: req.body.duration } }
         )
-        .then(newWorkout => {
-            res.json(newWorkout);
+        .then(data => {
+            res.json(data);
         })
         .catch(err => {
             res.status(400).json(err);
         }); 
     });
     
+    //getting all workouts in a range
     app.get("/api/workouts/range", (req, res) => {
         Workout.find({})
-            .then(dbWorkout => {
-                res.json(dbWorkout);
+            .then(data => {
+                res.json(data);
             })
             .catch(err => {
                 res.status(400).json(err);
